8ebb5f3d3af97aaa12830bd91fb09c918c45bdf7
Jonathan Young <jonyoungg@gmail.com>
2017-02-02 17:22:39 -0600

    fix spec file

--- js/charCountSpec.js
+++ js/charCountSpec.js
@@ -1,25 +1,21 @@
 var char = require("./charCount");
 
-console.log(char.charCount("aaabbc") === {
-  a : 3,
-  b : 2,
-  c : 1
-});
+console.log(char.charCount("aaabbc")['a'] === 3)
+console.log(char.charCount("aaabbc")['b'] === 2)
+console.log(char.charCount("aaabbc")['c'] === 1)
 
-console.log(char.charCount('rplyoacadawpettlleaodeeywapkniha') === {
-  a : 6,
-  e : 4,
-  l : 3,
-  p : 3,
-  w : 2,
-  d : 2,
-  o : 2,
-  t : 2,
-  y : 2,
-  k : 1,
-  h : 1,
-  i : 1,
-  c : 1,
-  n : 1,
-  r : 1
-});
+console.log(char.charCount('rplyoacadawpettlleaodeeywapkniha')['a'] === 6)
+console.log(char.charCount('rplyoacadawpettlleaodeeywapkniha')['e'] === 4)
+console.log(char.charCount('rplyoacadawpettlleaodeeywapkniha')['l'] === 3)
+console.log(char.charCount('rplyoacadawpettlleaodeeywapkniha')['p'] === 3)
+console.log(char.charCount('rplyoacadawpettlleaodeeywapkniha')['w'] === 2)
+console.log(char.charCount('rplyoacadawpettlleaodeeywapkniha')['d'] === 2)
+console.log(char.charCount('rplyoacadawpettlleaodeeywapkniha')['o'] === 2)
+console.log(char.charCount('rplyoacadawpettlleaodeeywapkniha')['t'] === 2)
+console.log(char.charCount('rplyoacadawpettlleaodeeywapkniha')['y'] === 2)
+console.log(char.charCount('rplyoacadawpettlleaodeeywapkniha')['k'] === 1)
+console.log(char.charCount('rplyoacadawpettlleaodeeywapkniha')['h'] === 1)
+console.log(char.charCount('rplyoacadawpettlleaodeeywapkniha')['i'] === 1)
+console.log(char.charCount('rplyoacadawpettlleaodeeywapkniha')['c'] === 1)
+console.log(char.charCount('rplyoacadawpettlleaodeeywapkniha')['n'] === 1)
+console.log(char.charCount('rplyoacadawpettlleaodeeywapkniha')['r'] === 1)
